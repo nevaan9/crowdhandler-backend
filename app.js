65b01cfcf05337a32cf58c9532c1aa8f95fc1f0a
Nevaan Perera <nevaan9@gmail.com>
2021-04-29 16:47:24 -0400

    use prettier

--- app.js
+++ app.js
@@ -8,88 +8,87 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-let chKey
-let configLoaded = false
+let chKey;
+let configLoaded = false;
 try {
-    const credsJson = require('./credentials/creds.json')
-    chKey = credsJson['chKey']
-    configLoaded = true
+  const credsJson = require("./credentials/creds.json");
+  chKey = credsJson["chKey"];
+  configLoaded = true;
 } catch (e) {
-    // Do nothing
+  // Do nothing
 }
 
 if (!chKey) {
-    chKey = process.env.CHKEY
+  chKey = process.env.CHKEY;
 }
 
-
-app.get('/', (req, res) => {
-  const msg = configLoaded ? 'HELLO LOCALHOST' : 'HELLO PRODUCTION'
-  const chKeyDetected = !!chKey
-  res.send(`${msg}: CH key detected: ${chKeyDetected}`)
-})
+app.get("/", (req, res) => {
+  const msg = configLoaded ? "HELLO LOCALHOST" : "HELLO PRODUCTION";
+  const chKeyDetected = !!chKey;
+  res.send(`${msg}: CH key detected: ${chKeyDetected}`);
+});
 
 // CROWD HANDLER
 app.post("/requests", async (req, res) => {
-    const userIP = req.ip === "::1" ? "127.0.0.1" : req.ip;
-    const userAgent = req.get("User-Agent");
-    const userLang = req.get("Accept-Language") || "en";
-    const targetUrl = req.body.targetUrl;
-    if (!targetUrl || !chKey) {
-      res.json({ message: 'Bad Request: Not target url or key specified' });
-      return
-    }
-    const crowdHandlerParams = {
-      url: targetUrl,
-      ip: userIP,
-      agent: userAgent,
-      lang: userLang,
-    };
-    const existingToken = req.query["ch-id"] || req.cookies["ch-id"] || null;
-    let result;
-    try {
-      if (existingToken) {
-        const { data } = await crowdhandlerRequest.get(
-          `requests/${existingToken}`,
-          crowdHandlerParams
-        );
-        result = data.result;
-      } else {
-        const { data } = await crowdhandlerRequest.post(
-          "requests",
-          crowdHandlerParams
-        );
-        result = data.result;
-      }
-    } catch ({ response }) {
-      console.log(response);
-      let redirectUrl = `https://wait.crowdhandler.com?url=${targetUrl}&ch-public-key=${chKey}`;
-      if (existingToken) {
-        redirectUrl = `${redirectUrl}&ch-id=${existingToken}`;
-      }
-      const encoded = encodeURI(redirectUrl);
-      res.json({ url: encoded, status: "redirect" });
-      return;
-    }
-    // Now handle the queue-ing
-    const cookie = existingToken || result.token || null;
-    if (result && result.promoted) {
-      res.json({ status: "pass", cookie });
+  const userIP = req.ip === "::1" ? "127.0.0.1" : req.ip;
+  const userAgent = req.get("User-Agent");
+  const userLang = req.get("Accept-Language") || "en";
+  const targetUrl = req.body.targetUrl;
+  if (!targetUrl || !chKey) {
+    res.json({ message: "Bad Request: Not target url or key specified" });
+    return;
+  }
+  const crowdHandlerParams = {
+    url: targetUrl,
+    ip: userIP,
+    agent: userAgent,
+    lang: userLang,
+  };
+  const existingToken = req.query["ch-id"] || req.cookies["ch-id"] || null;
+  let result;
+  try {
+    if (existingToken) {
+      const { data } = await crowdhandlerRequest.get(
+        `requests/${existingToken}`,
+        crowdHandlerParams
+      );
+      result = data.result;
     } else {
-      let redirectUrl = `https://wait.crowdhandler.com/${result.slug}?url=${targetUrl}`;
-      if (existingToken) {
-        redirectUrl = `${redirectUrl}&ch-id=${existingToken}`;
-      }
-      const encoded = encodeURI(redirectUrl);
-      res.json({ url: encoded, status: "redirect", cookie });
+      const { data } = await crowdhandlerRequest.post(
+        "requests",
+        crowdHandlerParams
+      );
+      result = data.result;
     }
-  });
+  } catch ({ response }) {
+    console.log(response);
+    let redirectUrl = `https://wait.crowdhandler.com?url=${targetUrl}&ch-public-key=${chKey}`;
+    if (existingToken) {
+      redirectUrl = `${redirectUrl}&ch-id=${existingToken}`;
+    }
+    const encoded = encodeURI(redirectUrl);
+    res.json({ url: encoded, status: "redirect" });
+    return;
+  }
+  // Now handle the queue-ing
+  const cookie = existingToken || result.token || null;
+  if (result && result.promoted) {
+    res.json({ status: "pass", cookie });
+  } else {
+    let redirectUrl = `https://wait.crowdhandler.com/${result.slug}?url=${targetUrl}`;
+    if (existingToken) {
+      redirectUrl = `${redirectUrl}&ch-id=${existingToken}`;
+    }
+    const encoded = encodeURI(redirectUrl);
+    res.json({ url: encoded, status: "redirect", cookie });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 // ========= FUNCTIONS ========
 const baseURL = "https://api.crowdhandler.com/v1";
